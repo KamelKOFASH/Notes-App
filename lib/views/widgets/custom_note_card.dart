a2157dccd73d21aecd2f083585801f07f3e837e9
Mustafa Kamel <mk8764687@gmail.com>
2024-09-04 23:50:03 +0300

    updates
    Refactored NoteCard widget and moved ListView to separate NotesListView widget

---
+++ lib/views/widgets/custom_note_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NoteCard extends StatelessWidget {
+  const NoteCard({super.key, required this.randomColor});
+  final Color randomColor;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shadowColor: Colors.black,
+      margin: const EdgeInsets.all(6),
+      color: randomColor,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+            title: const Text(
+              'Note Title',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                'description',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.check_mark_circled,
+                color: Colors.black,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.delete,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'May 15, 2023',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -3,29 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/widgets/custom_appbar.dart';
 import 'package:notes_app/views/widgets/custom_note_card.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         children: [
-          const CustomAppBar(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.9,
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: 12,
-              itemBuilder: (context, index) {
-                return NoteCard(
-                  randomColor: cardColors[index % cardColors.length],
-                );
-              },
-            ),
-          )
+          SizedBox(height: 40),
+          CustomAppBar(),
+          NotesListView(),
         ],
       ),
     );
