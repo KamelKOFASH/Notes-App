57f4d1347483a1c2af74bf996f8fb16926af6e38
Mustafa Kamel <mk8764687@gmail.com>
2024-09-05 19:54:47 +0300

    "Added Hive annotations to NoteModel and removed unnecessary import in CustomAppBar"

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,16 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+// flutter packages pub run build_runner build
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String description;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel({
     required this.title,
