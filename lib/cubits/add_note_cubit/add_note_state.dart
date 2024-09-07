d0c73fcc28b3c1cb5d46d27a01ff60792551d3a7
Mustafa Kamel <mk8764687@gmail.com>
2024-09-07 14:02:35 +0300

    Update add_note_state.dart
    "Added new states to AddNoteState: AddNoteLoading, AddNoteSuccess, and AddNoteFailed with error property"

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSuccess extends AddNoteState {}
+
+final class AddNoteFailed extends AddNoteState {
+  final String error;
+
+  AddNoteFailed(this.error);
+}
