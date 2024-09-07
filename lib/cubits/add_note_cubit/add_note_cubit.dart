4eb6c76473692b69b10324f2e91dac9f6fcb8d33
Mustafa Kamel <mk8764687@gmail.com>
2024-09-07 14:15:23 +0300

    Update add_note_cubit.dart
    "Updated AddNoteCubit to accept NoteModel, added Hive dependency, and implemented note addition with loading and error states"

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,13 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  void addNote() {
-    
-    emit(AddNoteSuccess());
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailed(e.toString()));
+    }
   }
 }
